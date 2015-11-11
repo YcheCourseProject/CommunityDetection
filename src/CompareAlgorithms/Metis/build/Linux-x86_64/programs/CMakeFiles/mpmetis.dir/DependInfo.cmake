751c18ce13eabe8fd29d88593bb4cfead46d6e21
Che Yulin <191836400@qq.com>
2015-11-11 16:09:08 +0800

    add metiscode

---
+++ src/CompareAlgorithms/Metis/build/Linux-x86_64/programs/CMakeFiles/mpmetis.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/cheyulin/ClionProjects/Community-detection/metis-5.1.0/programs/cmdline_mpmetis.c" "/home/cheyulin/ClionProjects/Community-detection/metis-5.1.0/build/Linux-x86_64/programs/CMakeFiles/mpmetis.dir/cmdline_mpmetis.c.o"
+  "/home/cheyulin/ClionProjects/Community-detection/metis-5.1.0/programs/io.c" "/home/cheyulin/ClionProjects/Community-detection/metis-5.1.0/build/Linux-x86_64/programs/CMakeFiles/mpmetis.dir/io.c.o"
+  "/home/cheyulin/ClionProjects/Community-detection/metis-5.1.0/programs/mpmetis.c" "/home/cheyulin/ClionProjects/Community-detection/metis-5.1.0/build/Linux-x86_64/programs/CMakeFiles/mpmetis.dir/mpmetis.c.o"
+  "/home/cheyulin/ClionProjects/Community-detection/metis-5.1.0/programs/stat.c" "/home/cheyulin/ClionProjects/Community-detection/metis-5.1.0/build/Linux-x86_64/programs/CMakeFiles/mpmetis.dir/stat.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "SVNINFO=\"\""
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/cheyulin/ClionProjects/Community-detection/metis-5.1.0/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../GKlib"
+  "../../include"
+  "../../programs/../libmetis"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
